0ddbe57ea07f7d700a8e125713bd4f4301343122
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-07-07 21:07:05 +0300

    added log

--- conftest.py
+++ conftest.py
@@ -2,28 +2,29 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import browser
 
 from utils import attach
 
 @pytest.fixture(scope='function')
-def setup_browser(request):
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "128.0",
         "selenoid:options": {
             "enableVNC": True,
+            "enableLOG": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
+        options=options)
 
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
+
     yield browser
 
     attach.add_screenshot(browser)
